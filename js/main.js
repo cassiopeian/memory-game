8fa37f2de5add4e6c9250a87a536bdb1f8388fec
cassiopeia <cassiopeia.neely@gmail.com>
2021-03-18 20:27:12 -0400

    Fisher-Yates shuffle
    Decided to replace my unreliable randomizer function with the Fisher-Yates shuffle method. So far, the camera card faces haven't been duplicated.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-let randomCard;
-
-cardFaces = [
+let cardFaces = [
     {
         image: 'images/card-faces/35mm-camera.svg'
     },
@@ -9,20 +7,28 @@ cardFaces = [
     },
 ];
 
-function randomizeCards(object) {
-    let arrKey = Object.keys(object);
-    return arrKey[Math.floor(Math.random() * arrKey.length)];
+function randomizeCards(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // while the array has unshuffled elements
+    while (0 !== currentIndex) {
+        // choose an unshuffled element
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // and replace it with the current element
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
 
-// $('.card-faces').each(function() {
-//     randomCard = cardFaces[randomizeCards(cardFaces)];
-//     $(this).attr('src', randomCard.image);
-// });
+randomizeCards(cardFaces);
 
-
+// for each array element, create a game tile
 cardFaces.forEach(element => {
-    element = cardFaces[randomizeCards(cardFaces)];
-    
     let gameTile = $('<figure class="tile"></figure>');
     let cardImg = $('<img class="card-faces" src="" />');
 
@@ -32,7 +38,32 @@ cardFaces.forEach(element => {
     $('#container').append(gameTile);
 });
 
+// when tiles are clicked, replace tile bg with the card face img
 $('.tile').on('click', function() {
     $(this).css('backgroundImage', 'none');
     $(this).children('img').css('display', 'block');
 });
+
+
+
+// function randomizeCards(object) {
+//     let arrKey = Object.keys(object);
+//     return arrKey[Math.floor(Math.random() * arrKey.length)];
+// }
+
+// $('.card-faces').each(function() {
+//     randomCard = cardFaces[randomizeCards(cardFaces)];
+//     $(this).attr('src', randomCard.image);
+// });
+
+// cardFaces.forEach(element => {
+//     element = cardFaces[randomizeCards(cardFaces)];
+    
+//     let gameTile = $('<figure class="tile"></figure>');
+//     let cardImg = $('<img class="card-faces" src="" />');
+
+//     $(cardImg).attr('src', element.image);  
+
+//     $(gameTile).append(cardImg);
+//     $('#container').append(gameTile);
+// });
