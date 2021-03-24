db9b5b6495fe583a32611a605237af42caa058f9
cassiopeia <cassiopeia.neely@gmail.com>
2021-03-24 18:59:21 -0400

    remove clutter
    Deleted commented-out code that didn't work.

--- js/main.js
+++ js/main.js
@@ -189,25 +189,3 @@ function pairChecker() {
         $('.match').children('img').css('display', 'block');
     }
 };
-
-// function randomizeCards(object) {
-//     let arrKey = Object.keys(object);
-//     return arrKey[Math.floor(Math.random() * arrKey.length)];
-// }
-
-// $('.card-faces').each(function() {
-//     randomCard = cardFaces[randomizeCards(cardFaces)];
-//     $(this).attr('src', randomCard.image);
-// });
-
-// cardFaces.forEach(element => {
-//     element = cardFaces[randomizeCards(cardFaces)];
-    
-//     let gameTile = $('<figure class="tile"></figure>');
-//     let cardImg = $('<img class="card-faces" src="" />');
-
-//     $(cardImg).attr('src', element.image);  
-
-//     $(gameTile).append(cardImg);
-//     $('#container').append(gameTile);
-// });
